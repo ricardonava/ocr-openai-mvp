4ced9695306f026f81e120157e3cc3a2dd686ef1
ricardonava <ricardo.a.nava@gmail.com>
2024-10-15 14:25:19 -0600

    chore: base integration of file uploader

--- apps/ui/src/pages/EmployeeDetailPage.tsx
+++ apps/ui/src/pages/EmployeeDetailPage.tsx
@@ -1,10 +1,21 @@
-import { Form, TextField, Submit } from '@just-scan/form';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { FileUploader, Form, Submit, TextField } from '@just-scan/form';
 import { useParams } from 'react-router-dom';
 
 const EmployeeDetailPage = () => {
   const { id } = useParams();
   return (
     <div className="px-32 space-y-16 divide-y">
+      <FileUploader
+        onSubmit={(data) => {
+          if (data.files) {
+            console.log('Submitting files: ', Array.from(data.files));
+          } else {
+            console.log('No files to submit');
+          }
+        }}
+      />
+
       <Form
         defaultValues={{
           firstName: '',
