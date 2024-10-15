39817019f42493b680fcbca73285af3026e4e936
Omar Sánchez <osanchez@justworks.com>
2024-10-15 13:30:52 -0600

    clean main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,8 +1,5 @@
 import express, { Request } from 'express';
 import * as path from 'path';
-import axios from 'axios';
-import * as fs from 'fs';
-import FormData from 'form-data';
 import multer from 'multer'; // For handling file uploads
 import { aiScan, encodeImage } from '@just-scan/ai-scan';
 
@@ -11,10 +8,9 @@ const app = express();
 // Set up file upload handling with multer
 const upload = multer({ dest: 'uploads/' });
 
-const openAIKey = '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'; // Store your OpenAI API key in environment variables for security.
 
 interface OpenAIResponse {
-  // Define your response structure here based on OpenAI's API documentation
+  //TODO
 }
 
 // Extend the Express Request interface to include files from multer
@@ -22,20 +18,18 @@ interface MulterRequest extends Request {
   files?: Express.Multer.File[];
 }
 
-// Function to handle the OpenAI API call
+// handle OpenAI API call
 async function uploadImagesAndGetResponse(pdfPaths: string[]): Promise<OpenAIResponse> {
   try {
-    const results: string[] = await Promise.all(
+    const results = await Promise.all(
       pdfPaths.map(async (pdfPath) => {
         const imageEncoded = await encodeImage(pdfPath); // Encode each image
         const result = await aiScan(imageEncoded, 'bankStatement'); // Scan and get the result
         return result; // Return the result for each PDF
       })
     );
-
-    // Concatenate the results
-    const concatenatedResult = results.join(' '); // or use any other separator if needed
-    return concatenatedResult as OpenAIResponse;
+    
+    return { results }; 
 
   } catch (error) {
     console.error('Error uploading PDFs to OpenAI:', error);
@@ -52,7 +46,7 @@ app.get('/api', (req, res) => {
 });
 
 
-// Route to upload PDF files and get a response from OpenAI
+// Route to upload images files and get a response from OpenAI
 app.post('/api/upload', upload.array('images', 10), async (req: MulterRequest, res) => {
   try {
     const images = req.files?.map((file) => file.path) || [];
@@ -63,8 +57,7 @@ app.post('/api/upload', upload.array('images', 10), async (req: MulterRequest, r
 
     // Call OpenAI API with uploaded PDF files
     const openAIResponse = await uploadImagesAndGetResponse(images);
-
-    // Return OpenAI's response to the client
+    
     res.json(openAIResponse);
   } catch (error) {
     console.error('Error during OpenAI processing:', error);
