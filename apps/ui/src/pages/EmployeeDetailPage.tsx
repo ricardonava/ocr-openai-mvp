fdd11901233539a91a2e0fbc40c5159d758069ef
ricardonava <ricardo.a.nava@gmail.com>
2024-10-15 13:29:33 -0600

    chore: base employee detail implementation with form

--- apps/ui/src/pages/EmployeeDetailPage.tsx
+++ apps/ui/src/pages/EmployeeDetailPage.tsx
@@ -1,9 +1,63 @@
-import React from 'react';
+import { Form, TextField, Submit } from '@just-scan/form';
 import { useParams } from 'react-router-dom';
 
 const EmployeeDetailPage = () => {
   const { id } = useParams();
-  return <div>Employee ID: {id}</div>;
+  return (
+    <div className="px-32 space-y-16 divide-y">
+      <Form
+        defaultValues={{
+          firstName: '',
+          lastName: '',
+          middleName: '',
+        }}
+        onSubmit={(data: any) => {
+          console.log(data);
+          console.log(id);
+        }}
+      >
+        <TextField name="firstName" label="First Name" placeholder="Ricardo" />
+        <TextField name="lastName" label="Last Name" placeholder="Aguirre" />
+        <TextField name="middleName" label="Middle Name" placeholder="Nava" />
+        <Submit />
+      </Form>
+
+      <Form
+        defaultValues={{
+          firstName: '',
+          lastName: '',
+          middleName: '',
+        }}
+        onSubmit={(data: any) => {
+          console.log(data);
+          console.log(id);
+        }}
+      >
+        <TextField name="firstName" label="First Name" placeholder="Ricardo" />
+        <TextField name="lastName" label="Last Name" placeholder="Aguirre" />
+        <TextField name="middleName" label="Middle Name" placeholder="Nava" />
+
+        <Submit />
+      </Form>
+
+      <Form
+        defaultValues={{
+          firstName: '',
+          lastName: '',
+          middleName: '',
+        }}
+        onSubmit={(data: any) => {
+          console.log(data);
+          console.log(id);
+        }}
+      >
+        <TextField name="firstName" label="First Name" placeholder="Ricardo" />
+        <TextField name="lastName" label="Last Name" placeholder="Aguirre" />
+        <TextField name="middleName" label="Middle Name" placeholder="Nava" />
+        <Submit />
+      </Form>
+    </div>
+  );
 };
 
 export default EmployeeDetailPage;
